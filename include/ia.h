e592b9e749d2fc828bf6a82f7bea506606cd647e
Arnaud VERSINI <Arnaud.VERSINI@gmail.com>
2010-01-17 15:20:19 +0100

    Ajout de la classe IA

---
+++ include/ia.h
@@ -0,0 +1,26 @@
+#ifndef IA_H_INCLUDED
+#define IA_H_INCLUDED
+#include "joueur.h"
+
+template<TJoueur joueur>
+class IAJoueur : public Joueur<joueur> {
+
+public:
+    IAJoueur() {
+        this->nom = nom;
+    }
+
+    inline size_t effectuerCoup() {
+        return 0;
+    }
+
+    inline virtual void prendreEnCompteCoupAdversaire(size_t colonne) {
+
+    }
+
+    inline virtual const string getJoueurInformations() {
+        return JeuxPuissanceQuatre::playerToString(joueur) + " joueur de type IA";
+    }
+
+}
+#endif // IA_H_INCLUDED
