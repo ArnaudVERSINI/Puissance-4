4b8e194fa83a66047c0ff4ef4b3e096bc49c8c3d
Arnaud VERSINI <Arnaud.VERSINI@gmail.com>
2010-01-18 20:53:57 +0100

    Ajout de commentaires sur le jeux du puissance 4.

--- include/jeu_puissance_4.hpp
+++ include/jeu_puissance_4.hpp
@@ -13,24 +13,46 @@ enum TJoueur {
     JOUEUR_NUL = 2
 };
 
+/**
+ * Représentation d'un jeux de puissance 4  et gérant les actions des joueurs.
+ */
 class JeuxPuissanceQuatre {
 
     private:
 
+    /**
+     * Le joueur actuel (bleu ou rouge).
+     */
     TJoueur joueurActuel;
+    /**
+     * Plateau de jeux permettant de conserver l'état de la partie.
+     */
     Plateau plateau;
 
+    /**
+     * Flag indiquand si la partie est terminé, utile vu que le nul est possible.
+      */
     bool partieFinie;
 
+    /*
+     * Joueur gagnant, information valide si la partie est finie.
+      */
     TJoueur gagnant;
 
-
+    /**
+     * Procédure de changement de joueur, utile entre les tours.
+    */
     inline void switchCurrentPlayer () {
         joueurActuel = joueurActuel == JOUEUR_BLEU ? JOUEUR_ROUGE : JOUEUR_BLEU;
     }
 
     public :
 
+    /**
+     * Tranformation d'un enum représentant une case en chaine de caractére.
+     * @param case_ Case à tranformer.
+     *  @return La chaine représentant la case.
+     */
     static inline const string caseToString(TCase case_) {
         const char* chaine = NULL;
         switch (case_) {
@@ -47,6 +69,11 @@ class JeuxPuissanceQuatre {
         return chaine;
     }
 
+    /**
+     * Tranformation d'un enum représentant un joueur en chaine de caractére.
+     * @param case_ Joueur à tranformer.
+     *  @return La chaine représentant le joueur.
+     */
     static inline const string playerToString(TJoueur joueur) {
         const char* chaine = NULL;
         switch (joueur) {
@@ -63,6 +90,9 @@ class JeuxPuissanceQuatre {
         return chaine;
     }
 
+    /**
+     * Construction d'un jeux avec plateau vide et joueur par defaut.
+     */
     inline JeuxPuissanceQuatre() {
         partieFinie = false;
         joueurActuel = JOUEUR_BLEU;
@@ -146,6 +176,10 @@ class JeuxPuissanceQuatre {
         }
     }
 
+    /**
+     * Fonction altérant l'état du jeux en jouant.
+     * @param colonne Colonne à jouer
+     * @return True si le jeux a été possible
     inline bool jouer(size_t colonne) {
         if (partieFinie) {
             return false;
@@ -161,26 +195,49 @@ class JeuxPuissanceQuatre {
         }
     }
 
+    /**
+     * Retourne le joueur actuel.
+     * @return Le joueur actuel.
     inline TJoueur getJoueurActuel() const {
         return joueurActuel;
     }
 
+    /**
+     * Retourne le joueur actuel sous forme de chaine affichable.
+     * @return Le joueur actuel sous forme de chaine de caractére.
+     */
     inline const string getJoueurActuelStr() const {
         return playerToString(joueurActuel);
     }
 
+    /**
+     * @return true si la partie est terminé
+     */
     inline bool isEnded() const {
         return partieFinie;
     }
 
+    /**
+     * Retourne la case dont les coordonnés ont été passés en parametres.
+     * @param ligne Ligne de la case à retourner
+     * @param  colonne Colonne de la ligne à afficher
+     * @return La valeur de la case
     inline TCase getCasePlateau(size_t ligne, size_t colonne) const {
         return plateau.get(ligne, colonne);
     }
 
+    /**
+     * Création d'une chaine de représentation du joueur actuel.
+      * @return Chaine du joueur
+      */
     inline const string getJoueurActuelGagnantStr() const {
         return playerToString(gagnant);
     }
 
+    /**
+     * Conversion du jeux en chaine de caractére pour affichage pour debugage.
+     * @return Le plateau en string
+     */
     inline const string toString() const {
         string retValue;
 
