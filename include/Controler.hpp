9ddadcf26a0509586b98b02fff36cef514aec133
FranckARRESTIER <f.arrestier@free.fr>
2010-01-24 23:53:27 +0800

    Ajout du controler

---
+++ include/Controler.hpp
@@ -0,0 +1,51 @@
+#ifndef CONTROLER_H_INCLUDED
+#define CONTROLER_H_INCLUDED
+
+#include "IHM_console.hpp"
+
+// include C++
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+
+
+class Controler {
+
+	JeuxPuissanceQuatre jeux;
+	IHMConsole console ;
+    Joueur<JOUEUR_BLEU>& joueurBleu;
+    Joueur<JOUEUR_ROUGE>& joueurRouge;
+
+
+public :
+
+
+
+
+
+    inline Controler(Joueur<JOUEUR_BLEU>& joueurBleu_, Joueur<JOUEUR_ROUGE>& joueurRouge_) :
+            joueurBleu(joueurBleu_),
+            joueurRouge(joueurRouge_){
+    	int colonne ;
+    	while (not jeux.isEnded()) {
+    		cout << jeux.getJoueurActuelStr() ;
+    		cin >> colonne ;
+    		jeux.jouer(colonne);
+    		//jeux.switchCurrentPlayer();
+    		console.print(jeux);
+    	}
+
+    	cout << "Le gagnant est :" << jeux.getJoueurActuelGagnantStr() << endl;
+
+    }
+
+    inline ~Controler() {
+
+    }
+};
+
+
+
+#endif // IHM_CONSOLE_H_INCLUDED
