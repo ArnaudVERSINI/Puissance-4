355f8b4195a65217039ca00792b03af2bfb04689
Arnaud VERSINI <Arnaud.VERSINI@gmail.com>
2010-01-31 17:28:18 +0100

    Avancement de l'IA.

--- include/ia.h
+++ include/ia.h
@@ -11,6 +11,8 @@ class IANode {
 template<TJoueur joueur_actuel, unsigned int profondeur>
 class IAJoueurMinMax : public Joueur<joueur_actuel> {
 
+
+
     /**
      * Le plateau actuel, conservé ici pour permettre de le modifier
      * lors du traitement de l'arbre
@@ -23,40 +25,69 @@ public:
         //this->nom = nom;
     }
 
-    inline size_t effectuerCoup() {
-        effectuerCoup(profondeur);
+    unsigned int effectuerCoup() {
+        return effectuerCoup(profondeur);
     }
-    inline size_t effectuerCoup(int profondeurActuelle) {
+
+    int calculScore() {
+        return 0;
+    }
+
+    size_t effectuerCoup(const unsigned int profondeurActuelle) {
 
         size_t max_colonne = 0;
-        int alpha = - std::numeric_limits<int>::max();
-        int beta = std::numeric_limits<int>::max();
+        const int betaInitial = numeric_limits<int>::max();
+        const int alphaInitial = - numeric_limits<int>::max();
+        int alpha = alphaInitial;
+
         if (profondeur > 0 && !plateauActuel.isPartieFinit()) {
             for(size_t colonne = 0; colonne < Plateau::LARGEUR; colonne++) {
                 if (plateauActuel.colonnePleine(colonne)) {
                     continue;
                 }
 
+                plateauActuel.addToColumn(colonne, (TCase) joueur_actuel);
+                int score_temp =  min(profondeurActuelle, alpha, betaInitial);
+                if (alpha < score_temp) {
+                    max_colonne = colonne;
+                    alpha = score_temp;
+                }
+                plateauActuel.supprimerCoup(colonne);
             }
         }
         return max_colonne;
     }
 
+    inline int min(const unsigned int profondeurActuelle, const int alpha, const int beta) {
+        if (profondeurActuelle == 0) {
+            return calculScore();
+        }
+
+        if (plateauActuel.isPartieFinit()) {
+            return calculScore();
+        }
+    }
+
+
+
+    inline int max(unsigned int profondeurActuelle, const int alpha, const int beta) {
+
+    }
+
+
     int evaluerGrille() {
         return 0;
     }
 
-    inline virtual void prendreEnCompteCoupAdversaire(size_t colonne) {
+    virtual void prendreEnCompteCoupAdversaire(size_t colonne) {
         plateauActuel.addToColumn(colonne, (TCase) inverseJoueur(joueur_actuel));
     }
 
-    inline virtual const string getJoueurInformations() {
+    virtual const string getJoueurInformations() {
         return JeuxPuissanceQuatre::playerToString(joueur_actuel) + " joueur de type IA";
     }
 
-    inline int calculScore() {
-        return 0;
-    }
+
 };
 
 void machinQuiSertARienMaisQuiGenereDuCode() {
