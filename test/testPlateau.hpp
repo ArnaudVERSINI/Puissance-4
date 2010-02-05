3c041190ffb2fbdfaac4bb9321498345d8712765
Arnaud VERSINI <Arnaud.VERSINI@gmail.com>
2010-02-06 00:17:07 +0100

    Utilisation de variables locales.

--- test/testPlateau.hpp
+++ test/testPlateau.hpp
@@ -20,73 +20,75 @@ public:
 
 private:
     void creation() {
-    	Plateau* plateau = new Plateau();
-        TEST_ASSERT(plateau->get(0, 0) == NONE)
+    	Plateau plateau;
+        TEST_ASSERT(plateau.get(0, 0) == NONE)
     }
     void setGet() {
-    	Plateau* plateau = new Plateau();
-            plateau->get(0,0);
+    	Plateau plateau;
+            plateau.get(0,0);
             // Test de la presence du jeton au bon endroit
-            TEST_ASSERT(plateau->get(0, 0) == NONE)
+            TEST_ASSERT(plateau.get(0, 0) == NONE)
 
             // Jeton BLUE en 0,0
-            plateau->set(0,0,BLUE);
-            TEST_ASSERT(plateau->get(0, 0) == BLUE)
+            plateau.set(0,0,BLUE);
+            TEST_ASSERT(plateau.get(0, 0) == BLUE)
 
             // Jeton BLUE en 0,0
-            plateau->set(3,3,RED);
-            TEST_ASSERT(plateau->get(3, 3) == RED)
+            plateau.set(3,3,RED);
+            TEST_ASSERT(plateau.get(3, 3) == RED)
 
         }
 
     void colonnePleine() {
-    	Plateau* plateau = new Plateau();
-    	TEST_ASSERT(plateau->colonnePleine(0) == false)
+    	Plateau plateau;
+    	TEST_ASSERT(plateau.colonnePleine(0) == false)
 
     	for(int ligne = 0 ; ligne <= Plateau::HAUTEUR + 3; ligne++) {
-    		plateau->set(ligne,3,RED);
+    		plateau.set(ligne,3,RED);
     	}
-    	TEST_ASSERT(plateau->colonnePleine(3) == true)
+    	TEST_ASSERT(plateau.colonnePleine(3) == true)
     }
 
     void addToColumn() {
-    	Plateau* plateau = new Plateau();
+    	Plateau plateau;
     	// Ajout d'un pion dans colonne 2
-    	TEST_ASSERT(plateau->addToColumn(2,RED) == 0)
+    	TEST_ASSERT(plateau.addToColumn(2,RED) == 0)
 
     	// Ajout d'un pion dans colonne 2
-    	TEST_ASSERT(plateau->addToColumn(2,BLUE) == 1)
+    	TEST_ASSERT(plateau.addToColumn(2,BLUE) == 1)
 
     	// Ajout d'un pion dans colonne 5
-    	TEST_ASSERT(plateau->addToColumn(5,BLUE) == 0)
+    	TEST_ASSERT(plateau.addToColumn(5,BLUE) == 0)
     }
 
     void aGagnerVertical() {
-        	Plateau* plateau = new Plateau();
+        {
+        	Plateau plateau ;
         	// Ajout d'un pion dans colonne 2
-        	plateau->addToColumn(0,RED);
-        	plateau->addToColumn(0,RED);
-        	plateau->addToColumn(0,RED);
+        	plateau.addToColumn(0,RED);
+        	plateau.addToColumn(0,RED);
+        	plateau.addToColumn(0,RED);
         	// Test attendu a faux
-        	TEST_ASSERT(plateau->aGagner(2,0,RED) == false)
+        	TEST_ASSERT(plateau.aGagner(2,0,RED) == false)
 
         	// Test attendu a faux
-        	plateau->addToColumn(0,RED);
-        	TEST_ASSERT(plateau->aGagner(3,0,RED) == true)
-
+        	plateau.addToColumn(0,RED);
+        	TEST_ASSERT(plateau.aGagner(3,0,RED) == true)
+        };
+        {
         	// Initialisation du plateau
-        	plateau = new Plateau();
+        	Plateau plateau;
 
-        	plateau->addToColumn(0,RED);
-        	plateau->addToColumn(0,BLUE);
-        	plateau->addToColumn(0,RED);
+        	plateau.addToColumn(0,RED);
+        	plateau.addToColumn(0,BLUE);
+        	plateau.addToColumn(0,RED);
 
         	// Test attendu a faux
-        	plateau->addToColumn(0,RED);
-        	TEST_ASSERT(plateau->aGagner(3,0,RED) == false)
-
+        	plateau.addToColumn(0,RED);
+        	TEST_ASSERT(plateau.aGagner(3,0,RED) == false)
 
         }
+    }
 
     void aGagnerHorizontal() {
            	Plateau* plateau = new Plateau();
@@ -111,32 +113,29 @@ private:
            	// Test attendu a faux
            	plateau->addToColumn(3,RED);
            	TEST_ASSERT(plateau->aGagner(0,3,RED) == false)
-
-
-           }
+    }
 
     void aGagnerDiagGaucheDroite() {
-           	Plateau* plateau = new Plateau();
+           	Plateau plateau;
            	// Ajout d'un pion dans colonne 2
-           	plateau->addToColumn(0,RED);
-        	plateau->addToColumn(1,BLUE);
-           	plateau->addToColumn(1,RED);
-           	plateau->addToColumn(2,BLUE);
-           	plateau->addToColumn(2,BLUE);
-           	plateau->addToColumn(2,RED);
+           	plateau.addToColumn(0,RED);
+        	plateau.addToColumn(1,BLUE);
+           	plateau.addToColumn(1,RED);
+           	plateau.addToColumn(2,BLUE);
+           	plateau.addToColumn(2,BLUE);
+           	plateau.addToColumn(2,RED);
            	// Test attendu a faux
-           	TEST_ASSERT(plateau->aGagner(2,2,RED) == false)
+           	TEST_ASSERT(plateau.aGagner(2,2,RED) == false)
 
            	// Test attendu a faux
-           	plateau->addToColumn(3,BLUE);
-           	plateau->addToColumn(3,BLUE);
-           	plateau->addToColumn(3,BLUE);
-           	plateau->addToColumn(3,RED);
-           	TEST_ASSERT(plateau->aGagner(3,3,RED) == true)
+           	plateau.addToColumn(3,BLUE);
+           	plateau.addToColumn(3,BLUE);
+           	plateau.addToColumn(3,BLUE);
+           	plateau.addToColumn(3,RED);
+           	TEST_ASSERT(plateau.aGagner(3,3,RED) == true)
 
 
-
-           }
+        }
 
 };
 
