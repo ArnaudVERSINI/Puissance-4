df9ccd0193852f7b2452efeb8b9be7ef549060a3
Franck <franck@franck-laptop.(none)>
2010-02-05 23:43:18 +0100

    Dev des tests : ajout des tests colonnePleine, setGet, addToColumn, aGagnerHorizontal, aGagnerVertical

--- test/testPlateau.hpp
+++ test/testPlateau.hpp
@@ -7,25 +7,137 @@ public:
     PlateauTestSuite()
     {
         TEST_ADD(PlateauTestSuite::creation)
-		TEST_ADD(PlateauTestSuite::addJeton)
+		TEST_ADD(PlateauTestSuite::setGet)
+		TEST_ADD(PlateauTestSuite::colonnePleine)
+		TEST_ADD(PlateauTestSuite::addToColumn)
+		TEST_ADD(PlateauTestSuite::aGagnerHorizontal)
+		TEST_ADD(PlateauTestSuite::aGagnerVertical)
+		TEST_ADD(PlateauTestSuite::aGagnerDiagGaucheDroite)
     }
+
+
+
 
 private:
     void creation() {
-        Plateau* plateau = new Plateau();
+    	Plateau* plateau = new Plateau();
         TEST_ASSERT(plateau->get(0, 0) == NONE)
     }
-    void addJeton() {
-            Plateau* plateau = new Plateau();
-            plateau->addToColumn(1,BLUE);
+    void setGet() {
+    	Plateau* plateau = new Plateau();
+            plateau->get(0,0);
             // Test de la presence du jeton au bon endroit
-            TEST_ASSERT(plateau->get(0, 1) == BLUE)
+            TEST_ASSERT(plateau->get(0, 0) == NONE)
 
-            //Est-il a cote ?
-            TEST_ASSERT(plateau->get(0, 0) == RED)
-            TEST_ASSERT(plateau->get(1, 1) == NONE)
-            TEST_ASSERT(plateau->get(0, 2) == NONE)
+            // Jeton BLUE en 0,0
+            plateau->set(0,0,BLUE);
+            TEST_ASSERT(plateau->get(0, 0) == BLUE)
+
+            // Jeton BLUE en 0,0
+            plateau->set(3,3,RED);
+            TEST_ASSERT(plateau->get(3, 3) == RED)
+
         }
+
+    void colonnePleine() {
+    	Plateau* plateau = new Plateau();
+    	TEST_ASSERT(plateau->colonnePleine(0) == false)
+
+    	for(int ligne = 0 ; ligne <= Plateau::HAUTEUR + 3; ligne++) {
+    		plateau->set(ligne,3,RED);
+    	}
+    	TEST_ASSERT(plateau->colonnePleine(3) == true)
+    }
+
+    void addToColumn() {
+    	Plateau* plateau = new Plateau();
+    	// Ajout d'un pion dans colonne 2
+    	TEST_ASSERT(plateau->addToColumn(2,RED) == 0)
+
+    	// Ajout d'un pion dans colonne 2
+    	TEST_ASSERT(plateau->addToColumn(2,BLUE) == 1)
+
+    	// Ajout d'un pion dans colonne 5
+    	TEST_ASSERT(plateau->addToColumn(5,BLUE) == 0)
+    }
+
+    void aGagnerVertical() {
+        	Plateau* plateau = new Plateau();
+        	// Ajout d'un pion dans colonne 2
+        	plateau->addToColumn(0,RED);
+        	plateau->addToColumn(0,RED);
+        	plateau->addToColumn(0,RED);
+        	// Test attendu a faux
+        	TEST_ASSERT(plateau->aGagner(2,0,RED) == false)
+
+        	// Test attendu a faux
+        	plateau->addToColumn(0,RED);
+        	TEST_ASSERT(plateau->aGagner(3,0,RED) == true)
+
+        	// Initialisation du plateau
+        	plateau = new Plateau();
+
+        	plateau->addToColumn(0,RED);
+        	plateau->addToColumn(0,BLUE);
+        	plateau->addToColumn(0,RED);
+
+        	// Test attendu a faux
+        	plateau->addToColumn(0,RED);
+        	TEST_ASSERT(plateau->aGagner(3,0,RED) == false)
+
+
+        }
+
+    void aGagnerHorizontal() {
+           	Plateau* plateau = new Plateau();
+           	// Ajout d'un pion dans colonne 2
+           	plateau->addToColumn(0,RED);
+           	plateau->addToColumn(1,RED);
+           	plateau->addToColumn(2,RED);
+           	// Test attendu a faux
+           	TEST_ASSERT(plateau->aGagner(0,2,RED) == false)
+
+           	// Test attendu a faux
+           	plateau->addToColumn(0,RED);
+           	TEST_ASSERT(plateau->aGagner(0,3,RED) == true)
+
+           	// Initialisation du plateau
+           	plateau = new Plateau();
+
+           	plateau->addToColumn(0,RED);
+           	plateau->addToColumn(1,BLUE);
+           	plateau->addToColumn(2,RED);
+
+           	// Test attendu a faux
+           	plateau->addToColumn(3,RED);
+           	TEST_ASSERT(plateau->aGagner(0,3,RED) == false)
+
+
+           }
+
+    void aGagnerDiagGaucheDroite() {
+           	Plateau* plateau = new Plateau();
+           	// Ajout d'un pion dans colonne 2
+           	plateau->addToColumn(0,RED);
+        	plateau->addToColumn(1,BLUE);
+           	plateau->addToColumn(1,RED);
+           	plateau->addToColumn(2,BLUE);
+           	plateau->addToColumn(2,BLUE);
+           	plateau->addToColumn(2,RED);
+           	// Test attendu a faux
+           	TEST_ASSERT(plateau->aGagner(2,2,RED) == false)
+
+           	// Test attendu a faux
+           	plateau->addToColumn(3,BLUE);
+           	plateau->addToColumn(3,BLUE);
+           	plateau->addToColumn(3,BLUE);
+           	plateau->addToColumn(3,RED);
+           	TEST_ASSERT(plateau->aGagner(3,3,RED) == true)
+
+
+
+           }
+
 };
 
 
