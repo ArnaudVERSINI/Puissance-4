27f31912718554b84e46bc3c11a04681012265d8
FranckARRESTIER <f.arrestier@gmail.com>
2010-02-24 22:19:27 +0100

    Remontée en conf de la partie graphique mais intégration de l'IA

--- include/IHM_sdl.hpp
+++ include/IHM_sdl.hpp
@@ -4,145 +4,147 @@
 #include <SDL/SDL.h>
 #include <iostream>
 
-
 #include "plateau.hpp"
-
+#include "jeu_puissance_4.hpp"
+#include "joueur.hpp"
 
 //Classe du jeu
-class Jeu
-{
+class Jeu {
+
+private:
+	//Le plateau jeu
+	TCase plateau[Plateau::HAUTEUR][Plateau::LARGEUR];
+
+	//Surfaces d'un rond, d'une croix et d'un fond
+	SDL_Surface *o, *x, *bg;
+
+	//Variable pour un tour
+	TCase tour;
+
+	JeuxPuissanceQuatre jeux;
+
+public:
+	static const size_t WIDTH = 392;
+	static const size_t HEIGHT = 336;
+	Jeu() {
+		int i, j;
+
+		//On met toutes les cases � Vide
+		for (i = 0; i < Plateau::HAUTEUR; i++)
+			for (j = 0; j < Plateau::LARGEUR; j++)
+				plateau[i][j] = NONE;
+
+		//Valeur par d�faut pour les surfaces
+		o = NULL;
+		x = NULL;
+		bg = NULL;
+	}
+
+	~Jeu() {
+		//On lib�re les surfaces
+		SDL_FreeSurface(o);
+		SDL_FreeSurface(bg);
+		SDL_FreeSurface(x);
+	}
+
+	bool init() {
+		//V�rification de l'allocation des surfaces
+		if (o != NULL) {
+			SDL_FreeSurface(o), o = NULL;
+		}
+		if (x != NULL) {
+			SDL_FreeSurface(x), x = NULL;
+		}
+		if (bg != NULL) {
+			SDL_FreeSurface(bg), bg = NULL;
+		}
+
+		//On charge toutes les images dans les surfaces associ�es
+		o = SDL_LoadBMP("ressources/rouge.bmp");
+		x = SDL_LoadBMP("ressources/jaune.bmp");
+		bg = SDL_LoadBMP("ressources/fond.bmp");
+
+		//On teste le retour du chargement
+		if ((o == NULL) || (x == NULL) || (bg == NULL)) {
+			cout << "Probleme de chargement du O, du X ou de l'image de fond"
+					<< endl;
+			return false;
+		}
+
+		//On initialise le premier tour: ce sera Rond qui commencera
+		tour = RED;
+
+		//Mis en place de la transparence
+		if (SDL_SetColorKey(o, SDL_SRCCOLORKEY, 0) == -1)
+			cout << "Erreur avec la transparence du rond" << endl;
+
+		if (SDL_SetColorKey(x, SDL_SRCCOLORKEY, 0) == -1)
+			cout << "Erreur avec la transparence de la croix" << endl;
+
+		return true;
+	}
+
+	void clic(int x, int y) {
+		size_t i, j;
+		size_t colonne, ligne;
+
+		//On recupere la largeur et l'hauteur d'une case
+		colonne = WIDTH / Plateau::LARGEUR;
+		ligne = HEIGHT / Plateau::HAUTEUR;
+
+		//Calcul de la case associée
+		i = y / ligne;
+		j = x / colonne;
+
+		cout << "Colonne  : " << j << endl;
+
+		//Si la case est vide, on met � jour son type et la variable tour
 
 
-    private:
-    	//Le plateau jeu
-    	TCase plateau[Plateau::HAUTEUR][Plateau::LARGEUR];
+		TJoueur joueurActuel = jeux.getJoueurActuel();
 
-    	//Surfaces d'un rond, d'une croix et d'un fond
-    	SDL_Surface *o, *x, *bg;
+		if (j >= Plateau::LARGEUR) {
+			return;
+		}
+		if (!jeux.estColonneJouable(j)) {
+			return;
+		}
 
-    	//Variable pour un tour
-    	TCase tour;
+		i = Plateau::HAUTEUR - jeux.jouer(j) - 1;
+		cout << "Ligne  : " << i << endl;
+		//Si la case est vide, on met à jour son type et la variable tour
+		if (plateau[i][j] == NONE) {
+			plateau[i][j] = tour;
+			tour = (tour == RED) ? BLUE : RED;
+		}
 
-    public:
-    	static const size_t WIDTH=392;
-    	static const size_t HEIGHT=336;
-    	Jeu()
-    	{
-    	    int i,j;
+	}
 
-    	    //On met toutes les cases � Vide
-    		for(i=0;i<Plateau::HAUTEUR;i++)
-    			for(j=0;j<Plateau::LARGEUR;j++)
-    				plateau[i][j] = NONE;
+	void aff(SDL_Surface *screen) {
+		//Le couple (w,h) représentera les dimensions d'une case du plateau
+		int w = WIDTH / Plateau::LARGEUR, h = HEIGHT / Plateau::HAUTEUR, i, j,
+				k, l;
+		SDL_Rect r = { 0, 0, 0, 0 };
 
-    	    //Valeur par d�faut pour les surfaces
-    	    o=NULL;
-    	    x=NULL;
-    	    bg=NULL;
-    	}
+		//Dessiner le fond d'ecran
+		SDL_BlitSurface(bg, NULL, screen, &r);
 
-    	~Jeu()
-    	{
-    	    //On lib�re les surfaces
-    	    SDL_FreeSurface(o);
-    	    SDL_FreeSurface(bg);
-    	    SDL_FreeSurface(x);
-    	}
+		//On parcourt les cases du tableau, r sera le SDL_Rect qui représentera la position de la case courante
+		for (k = 0, i = 0; i < HEIGHT; i += h, k++) {
+			r.y = i;
+			for (j = 0, l = 0; j < WIDTH; j += w, l++) {
+				r.x = j;
 
-    	bool init()
-    	{
-    	    //V�rification de l'allocation des surfaces
-    	    if(o!=NULL)
-    	        {
-    	            SDL_FreeSurface(o), o = NULL;
-    	        }
-    	    if(x!=NULL)
-    	        {
-    	            SDL_FreeSurface(x), x = NULL;
-    	        }
-    	    if(bg!=NULL)
-    	        {
-    	            SDL_FreeSurface(bg), bg = NULL;
-    	        }
+				//On dessine en fonction du type de la case
+				if (plateau[k][l] == BLUE) {
+					SDL_BlitSurface(x, NULL, screen, &r);
+				} else if (plateau[k][l] == RED) {
+					SDL_BlitSurface(o, NULL, screen, &r);
+				}
+			}
+		}
 
-    	    //On charge toutes les images dans les surfaces associ�es
-    	    o = SDL_LoadBMP("ressources/rouge.bmp");
-    	    x = SDL_LoadBMP("ressources/jaune.bmp");
-    	    bg = SDL_LoadBMP("ressources/fond.bmp");
-
-    	    //On teste le retour du chargement
-    	    if( (o==NULL) || (x==NULL) || (bg==NULL))
-    	    	{
-    	    	cout << "Probleme de chargement du O, du X ou de l'image de fond" << endl;
-    	    	return false;
-    	    	}
-
-    	    //On initialise le premier tour: ce sera Rond qui commencera
-    	    tour = RED;
-
-    	    //Mis en place de la transparence
-    	    if(SDL_SetColorKey(o,SDL_SRCCOLORKEY,0)==-1)
-    	        cout << "Erreur avec la transparence du rond" << endl;
-
-    	    if(SDL_SetColorKey(x,SDL_SRCCOLORKEY,0)==-1)
-    	        cout << "Erreur avec la transparence de la croix" << endl;
-
-    	    return true;
-    	}
-
-    	void clic(int x, int y)
-    	{
-    	    int j;
-    	    int w;
-
-    	    //On recupere la colonne ou le clic a eu lieu
-    	    w = WIDTH/Plateau::LARGEUR;
-
-
-    	    //Calcul de la case associ�e
-
-    	    j = x/w;
-
-    	    cout << "Colonne  : " << j << endl;
-    	    /*//Si la case est vide, on met � jour son type et la variable tour
-    	    if(plateau[i][j]==NONE)
-    	    	{
-    		plateau[i][j] = tour;
-    		tour = (tour==RED)?BLUE:RED;
-    		}*/
-
-    	}
-
-    	void aff(SDL_Surface *screen)
-    	{
-    	    //Le couple (w,h) repr�sentera les dimensions d'une case du plateau
-    	    int w = WIDTH/Plateau::LARGEUR, h=HEIGHT/Plateau::HAUTEUR,i,j,k,l;
-    	    SDL_Rect r = { 0,0,0,0 };
-
-    	    //Dessiner le fond d'ecran
-    	    SDL_BlitSurface(bg,NULL,screen,&r);
-
-    	    //On parcourt les cases du tableau, r sera le SDL_Rect qui repr�sentera la position de la case courante
-    	    for(k=0,i=0;i<HEIGHT;i+=h,k++)
-    	    	{
-    	    	r.y = i;
-    	    	for(j=0,l=0;j<WIDTH;j+=w,l++)
-    	    	    {
-    	    	    r.x = j;
-
-    	    	    //On dessine en fonction du type de la case
-    	    	    if(plateau[k][l]==BLUE)
-    	    	        {
-    	    	        SDL_BlitSurface(x, NULL, screen,&r);
-    	    	        }
-    	    	    else if(plateau[k][l]==RED)
-    	        	{
-    	    	        SDL_BlitSurface(o, NULL, screen,&r);
-    	    	        }
-    	            }
-    	        }
-    	}
+	}
 };
 #endif
-
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,85 +1,72 @@
-/*
-Programme qui ouvre une fen�tre SDL
-Program that opens a SDL window
-Copyright (C) 2006 BEYLER Jean Christophe
-
-This program is free software; you can redistribute it and/or
-modify it under the terms of the GNU General Public License
-as published by the Free Software Foundation; either version 2
-of the License, or (at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
-*/
-
 #include <SDL/SDL.h>
 #include <iostream>
+#include "IHM_sdl.hpp"
+#include "jeu_puissance_4.hpp"
+#include "joueur.hpp"
 
+Jeu jeu;
 
-const int WIDTH=640;
-const int HEIGHT=480;
+int jeuDeuxJoueursHumain() {
 
-int main(int argc, char **argv)
-{
-    //D�claration des variables
-    SDL_Event event;
-    SDL_Surface *screen;
-    int done = 0;
+	JoueurHumain<JOUEUR_BLEU> joueur1;//("Arnaud");
+	JoueurHumain<JOUEUR_ROUGE> joueur2;//("Franck");
 
-    //Initialisation de SDL
-    if(SDL_Init(SDL_INIT_VIDEO)!=0)
-    {
-        std::cerr << "Probleme pour initialiser SDL" << SDL_GetError() << std::endl;
-        return 1;
-    }
+	SDL_Event event;
+	SDL_Surface *screen;
+	int done = 0;
 
-    //Mettre un titre � la fen�tre
-    SDL_WM_SetCaption("Programme SDL de base", NULL);
-    //Ouvrerture d'une surface pour l'affichage de la fen�tre
-    screen = SDL_SetVideoMode(WIDTH,HEIGHT, 32,
-                              SDL_DOUBLEBUF | SDL_HWSURFACE);
-    if(screen==NULL)
-        done = 1;
-    //Boucle generale
-    while(!done)
-    {
-        //Traiter les �v�nements
-        while(SDL_PollEvent(&event))
-        {
-            switch(event.type)
-            {
-                //Si on veut quitter, done=1 suffira
-                case SDL_QUIT:
-                done=1;
-                break;
-                //Si on vient de relacher une touche de clavier
-                case SDL_KEYUP:
-                //Et si c'est la lettre q
-		/*
-		 Attention, ceci ne fonctionne pas avec tous les Syst�mes d'exploitation et tous les claviers
-		 Pour le rendre portable, il faudrait passer par les Unicodes, mais cela sort du cadre de ce tutoriel
-		 d'introduction.
-		 Si vous voulez utiliser la lettre 'q' pour quitter et ceci ne fonctionne pas, alors c'est la lettre
-		 'a' qu'il faudra utiliser (c'est la place de la lettre 'q' sur le clavier QWERTY).
-		 Si vous voulez vraiment la lettre 'q', remplacer SDLK_q par SDLK_a, cela devrait faire l'affaire
-		 */
-                if(event.key.keysym.sym==SDLK_q)
-                    //On met done a 1 pour quitter
-                    done=1;
-                break;
-                //Sinon on ne fait rien
-                default:
-                break;
-            }
-        }
-    }
+	//Initialisation
+	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
+		cout << "Probleme pour initialiser SDL: " << SDL_GetError() << endl;
+		return 1;
+	}
 
-    SDL_Quit();
-    return 0;
+	//Mettre un titre � la fen�tre
+	SDL_WM_SetCaption("Morpion Version 1.0", NULL);
+
+	//Ouvrir une fenetre
+	screen = SDL_SetVideoMode(Jeu::WIDTH, Jeu::HEIGHT, 16, SDL_DOUBLEBUF
+			| SDL_HWSURFACE);
+	if (screen == NULL)
+		done = 1;
+
+	//Initialiser le jeu
+	if (!jeu.init())
+		return 1;
+
+	//Boucle generale
+	while (!done) {
+		//Traiter les evenements
+		while (SDL_PollEvent(&event)) {
+			switch (event.type) {
+			case SDL_QUIT:
+				done = 1;
+				break;
+			case SDL_KEYUP:
+				if (event.key.keysym.sym == SDLK_q)
+					done = 1;
+				break;
+			case SDL_MOUSEBUTTONUP:
+				jeu.clic(event.button.x, event.button.y);
+				break;
+			default:
+				break;
+			}
+		}
+
+		jeu.aff(screen);
+		SDL_Flip(screen);
+	}
+
+	SDL_Quit();
+
+}
+
+int main(int argc, char **argv) {
+
+	jeuDeuxJoueursHumain();
+	(void) argc;
+	(void) argv;
+
+	return 0;
 }
