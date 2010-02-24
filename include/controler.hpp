dfbaefd758ddd9d04cf3bf2d0bc56777b3768ed7
FranckARRESTIER <f.arrestier@gmail.com>
2010-02-24 22:15:23 +0100

    Suppression du code commenté

--- include/controler.hpp
+++ include/controler.hpp
@@ -49,29 +49,6 @@ public :
     inline void launchGame() {
 
     	while (not jeux.isEnded()) {
-
-    		 //do {
-					    //cout << jeux.getJoueurActuelStr() << " : ";
-    		            //cout << "veuillez saisir le numero de colonne où jouer ( de 0 à " << Plateau::LARGEUR << "): ";
-    		            //cin >> colonne;
-    		            //cout << endl;
-    		            //cout << colonne << endl;
-    		            //if ( colonne >= Plateau::LARGEUR) {
-    		            //    cout << "Merci de saisir un numero entre 0 et " << Plateau::LARGEUR << endl;
-    		            //}
-    		            //else if (jeux.estColonneJouable(colonne) == false) {
-    		            //	cout << "Cette colonne " << colonne << " est pleine. Veuillez en choisir une autre"<< endl;
-    		            //}
-    		            //else {
-    		            //	jouable = true ;
-    		            //}
-
-    		        //} while (colonne != true);
-
-
-
-    		//jeux.jouer(colonne);
-    		//jeux.switchCurrentPlayer();
     		traitementTour();
     		console.print(jeux);
     	}
